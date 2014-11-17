2c576f4d2d484201f820a36e7f233ce9282b3bbe
gouki04 <gouki04@126.com>
2014-11-17 08:19:51 +0800

    添加cocos2dx和jsoncpp的依赖

---
+++ cocos2dx/include/platform/win32/Win32KeyboardMgr.h
@@ -0,0 +1,10 @@
+/****************************************************************************
+Wirtten by gouki04
+****************************************************************************/
+
+#ifndef Win32KeyboardMgr_h__
+#define Win32KeyboardMgr_h__
+
+class Win32KeyboardMgr
+
+#endif // Win32KeyboardMgr_h__
